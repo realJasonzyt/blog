13b5ea8ef0085c3ace23b2db00cf05fda49258cf
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2024-04-20 03:05:43 +0800

    feat: add source

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="app">
+    <el-container>
+      <el-header> </el-header>
+      <el-main id="main">
+        <router-view />
+      </el-main>
+      <el-footer>
+        <p>© 2023</p>
+      </el-footer>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+#main {
+  max-width: 1280px;
+}
+</style>
