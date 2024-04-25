2976306e4f9878313a04ccfdfdd6b30383be79a1
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2024-04-26 04:20:23 +0800

    chore: add JSX plugin & use vue.esm-bundler

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import VueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools(), VueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      vue: 'vue/dist/vue.esm-bundler.js'
     }
   }
 })
