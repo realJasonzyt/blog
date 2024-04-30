d072cb0d54281b0f320560c826fbdc379f3191e1
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2024-05-01 04:02:23 +0800

    deps: add unplugin-vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import VueJsx from '@vitejs/plugin-vue-jsx'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools(), VueJsx()],
+  plugins: [Vue(), VueDevTools(), VueJsx(), VueRouter()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
