4c6516532693c26d0e42882c323c1452a6e70c01
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2024-05-02 16:58:24 +0800

    feat: add pages

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+import { ref } from 'vue'
+import { RouterView, useRouter } from 'vue-router/auto'
+
+const router = useRouter()
+
+let barPinned = ref(false)
+
+const handleNavBarSelect = (index: string) => {
+  switch (index) {
+    case 'logo':
+      router.push('/')
+      break
+    case 'articles':
+      router.push('/articles')
+      break
+    default:
+      break
+  }
+}
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/') {
+    barPinned.value = false
+  } else {
+    barPinned.value = true
+  }
+  next()
+})
+</script>
 
 <template>
-  <div id="app">
-    <el-container>
-      <el-header> </el-header>
-      <el-main id="main">
-        <router-view />
-      </el-main>
-      <el-footer>
-        <p>© 2023</p>
-      </el-footer>
-    </el-container>
-  </div>
+  <NavBar :handle-select="handleNavBarSelect" :pinned="barPinned" />
+  <RouterView />
 </template>
 
-<style scoped>
-#main {
-  max-width: 1280px;
-}
-</style>
+<style scoped></style>
---
+++ src/pages/articles.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="main">
+    <h1>Articles</h1>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  padding: 20px;
+}
+</style>
