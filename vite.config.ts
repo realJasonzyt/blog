3b811f9fee0684403c5b0abb0ae772ae3cfc5a34
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2024-05-12 02:12:44 +0800

    deps: new dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import Vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import VueJsx from '@vitejs/plugin-vue-jsx'
 import VueRouter from 'unplugin-vue-router/vite'
+import VueMdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Vue(), VueDevTools(), VueJsx(), VueRouter()],
+  plugins: [Vue(), VueDevTools(), VueJsx(), VueRouter(), VueMdx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
