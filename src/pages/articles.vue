320e12dc025db1919720985413bd9b1041dd8cda
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2024-05-12 20:26:56 +0800

    feat: add TheFooter to pages

--- src/pages/articles.vue
+++ src/pages/articles.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import TheFooter from '@/components/TheFooter.vue'
+</script>
+
 <template>
   <RouterView />
+  <TheFooter />
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
 import TheWelcome from '@/components/TheWelcome.vue'
+import TheFooter from '@/components/TheFooter.vue'
 </script>
 
 <template>
   <TheWelcome />
+  <div class="bg-placeholder" />
+  <TheFooter />
 </template>
 
-<style scoped></style>
+<style scoped>
+.bg-placeholder {
+  display: block;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+</style>
