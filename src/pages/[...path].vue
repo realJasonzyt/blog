6caf61152fe9fa1bc319b592e6dec2809fb90a05
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2024-10-20 23:06:01 +0800

    feat: add 404 page

---
+++ src/pages/[...path].vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="main">
+    <p class="statuscode">404</p>
+    <p class="status">Not Found</p>
+    <RouterLink to="/"><el-button class="back-btn">Go back to Home</el-button></RouterLink>
+  </div>
+  <!-- TODO: Suggest random photo in gallery -->
+</template>
+
+<style scoped>
+.main {
+  padding: 2rem;
+  height: 100vh;
+  text-align: center;
+}
+
+.statuscode {
+  font-size: 10rem;
+  font-family: 'Ubuntu';
+  color: #333;
+}
+
+.status {
+  font-size: 3rem;
+  color: #444;
+}
+
+.back-btn {
+  margin-top: 1rem;
+}
+</style>
