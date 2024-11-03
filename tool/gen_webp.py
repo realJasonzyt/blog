4633f1bcecfccb4e1d0baa463ab56706aa60e515
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2024-11-03 21:51:20 +0800

    chore: update gen_webp

--- tool/gen_webp.py
+++ tool/gen_webp.py
@@ -1,7 +1,13 @@
 import os
+import sys
+
+if len(sys.argv) > 1:
+  os.chdir(sys.argv[1])
 
 # compress all jpegs in the current directory
-for filename in os.listdir("."):
-    if filename.endswith(".jpg"):
-        os.system("ffmpeg -i " + filename + " -lossless 0 " + filename[:-4] + "_m.webp")
-        os.system("ffmpeg -i " + filename + " -lossless 0 -vf scale=iw/2:ih/2 " + filename[:-4] + "_s.webp")
+for filename in os.listdir('.'):
+  if filename.endswith(".jpg"):
+    if not os.path.isfile(filename[:-4] + "_m.webp"):
+      os.system("ffmpeg -i " + filename + " -lossless 0 " + filename[:-4] + "_m.webp")
+    if not os.path.isfile(filename[:-4] + "_s.webp"):
+      os.system("ffmpeg -i " + filename + " -lossless 0 -vf scale=iw/2:ih/2 " + filename[:-4] + "_s.webp")
