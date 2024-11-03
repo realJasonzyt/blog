6ca46a31b0a360117d1daefa0e1115122921031a
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2024-11-03 17:52:36 +0800

    feat: implement hwaterfall

---
+++ src/components/GalleryPhoto.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import type { Photo } from '@/scripts/gallery';
+import { ref } from 'vue';
+
+defineProps<{ photo: Photo }>()
+defineEmits(['click'])
+
+let showInfo = ref(false)
+
+</script>
+
+<template>
+  <div class="photo-card">
+    <img :src="photo.url.s" :alt="photo.description" class="photo" @click="$emit('click', photo)"
+      @mouseenter="showInfo = true" @mouseleave="showInfo = false" />
+    <div class="info" :style="{ opacity: showInfo ? 1 : 0 }">
+      {{ photo.description }}
+      <!-- <span class="album">
+        <el-icon>
+          <IconImage />
+        </el-icon>
+        {{ photo.album }}
+      </span> -->
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.photo-card {
+  height: 300px;
+  display: inline-block;
+  margin-bottom: 20px;
+}
+
+.photo {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 15px;
+  border: 1px solid #eee;
+  border-radius: 15px;
+  transition: box-shadow 0.3s ease-in-out;
+  z-index: 0;
+  display: inline-block;
+  overflow: hidden;
+  position: relative;
+}
+
+.photo:hover {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+}
+
+.info {
+  text-align: right;
+  padding: 0 10px 0 0;
+  margin-top: -36px;
+  border-radius: 15px;
+  z-index: 1;
+  border-top: none;
+  color: #eee;
+  transition: opacity 0.3s ease-in-out;
+}
+</style>
---
+++ src/components/HorizontalWaterfall.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+
+const props = defineProps<{ height: number }>()
+
+setTimeout(() => {
+
+  let mainDiv = document.querySelector('div.wf-main')
+  if (!mainDiv) {
+    console.log("failed to find main div")
+  } else {
+    let gutter = 10
+    let width = mainDiv.clientWidth
+    let children = mainDiv.children
+
+    let curLineWidth = 0
+    let curLine = []
+    let childrenWidths = []
+
+    for (let i = 0; i < children.length; i++) {
+      let child = children[i] as HTMLElement
+      let w = child.clientWidth
+      let h = child.clientHeight
+      let w1 = (w * props.height / h)
+      curLine.push(child)
+      childrenWidths.push(w1)
+
+      curLineWidth += w1 + gutter
+      if (curLineWidth >= width) {
+        for (let j = 0; j < curLine.length; j++) {
+          let w = childrenWidths[j]
+          let realContainerWidth = width - gutter * (j + 1)
+          let calcWidth = w / curLineWidth * realContainerWidth
+          curLine[j].style.width = calcWidth + 'px'
+        }
+        curLineWidth = 0
+        curLine = []
+        childrenWidths = []
+      }
+      // process the last line, if it is nearly full
+      if (i === children.length - 1 && curLineWidth > 0.7 * width) {
+        for (let j = 0; j < curLine.length; j++) {
+          let w = childrenWidths[j]
+          let realContainerWidth = width - gutter * (j + 1)
+          let calcWidth = w / curLineWidth * realContainerWidth
+          curLine[j].style.width = calcWidth + 'px'
+        }
+      }
+    }
+  }
+}, 100);
+</script>
+
+<template>
+  <div class="wf-main">
+    <slot></slot>
+  </div>
+</template>
