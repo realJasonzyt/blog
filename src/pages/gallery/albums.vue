b9b522940b1966b6fb1ad6531ae5fd00833fe79f
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2024-12-30 02:38:15 +0800

    feat: remove lagacy code

--- src/pages/gallery/albums.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import GalleryAlbum from '@/components/GalleryAlbum.vue';
-import { getAlbums } from '@/utils/gallery';
-</script>
-
-<template>
-  <!-- TODO: Sort albums by time -->
-  <GalleryAlbum v-for="a in getAlbums()" :album="a" :id="a.id"></GalleryAlbum>
-</template>
--- src/pages/gallery/categories.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts" setup>
-import GalleryCategory from '@/components/GalleryCategory.vue';
-import { getCategories } from '@/utils/gallery';
-</script>
-
-<template>
-  <GalleryCategory v-for="c in getCategories()" :category="c"></GalleryCategory>
-</template>
