74d9278764568f8b850b7280e97e7d3420a5bb54
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-01-01 12:28:05 +0800

    feat: under construction title

--- src/components/nav/BarTitle.vue
+++ src/components/nav/BarTitle.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="title select-none font-mono font-bold text-lg" :class="show ? 'show' : 'hidden'">
     <slot></slot>
+    <span class="ml-2 text-sm text-gray-600 align-text-bottom">(Under Construction)</span>
   </div>
 </template>
 
@@ -12,7 +13,7 @@ defineProps({
 
 <style scoped>
 .title {
-  color: rgb(var(--color-gray-600));
+  color: rgb(var(--color-gray-700));
   transition: opacity 0.4s ease-in-out;
 }
 </style>
